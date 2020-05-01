c066ec78d02bc54f3f6e8a02c8880f329a9e5c79
武松波 <camelwu@126.com>
2020-05-01 16:52:26 +0800

    fix: px2rem adaptor mobile

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,14 @@
 // vue.config.js
 module.exports = {
-    chainWebpack: config => {
-        config.module
-            .rule('css')
-            .test(/\.css$/)
-            .oneOf('vue')
-            .resourceQuery(/\?vue/)
-            .use('px2rem')
-            .loader('px2rem-loader')
-            .options({
-                remUnit: 75
-            })
+    lintOnSave: false,//关闭eslint
+    css: {
+        loaderOptions: {
+            postcss: {
+                plugins: [
+                    require('postcss-px2rem')({remUnit: 75}), // 换算的基数
+                ]
+            }
+        }
     }
 }
+// remPrecision：rem小数点后精度
