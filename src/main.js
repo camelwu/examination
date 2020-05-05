406fbec413df460546410449d995f5d5517d78fa
武松波 <camelwu@126.com>
2020-05-05 22:10:13 +0800

    fix: remove js-native-bridge

--- src/main.js
+++ src/main.js
@@ -1,16 +1,14 @@
 import Vue from 'vue'
-import Bridge from './plugins/bridge'
-import JsBridge from 'js-native-bridge'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'lib-flexible/flexible'
 import 'normalize.css/normalize.css'
 import './assets/global.css'
-import 'lib-flexible/flexible'
+import Bridge from './plugins/bridge'
 
 Vue.config.productionTip = false
 Vue.prototype.$bridge = Bridge
-Vue.prototype.$jsbridge = JsBridge
 
 new Vue({
     router,
